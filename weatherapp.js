4690b524276c9ca16ca4384c18ea851adbcb6d20
SlimSlim94 <slimjim3094@gmail.com>
2021-12-05 16:31:58 -0500

    updating weather app locations

--- weatherapp.js
+++ weatherapp.js
@@ -1,37 +1,39 @@
-$(document).ready(function () {
+$(document).ready(function(){
 
-    // Get Location
+    //Get Location
     navigator.geolocation.getCurrentPosition(success, error);
 
-    function success(pos) {
+    function success(pos){
         var lat = pos.coords.latitude;
         var long = pos.coords.longitude;
         weather(lat, long);
     }
 
-    function error() {
+    function error(){
         console.log('There was an error');
     }
 
-    // Call Weather
-    function weather(lat, long) {
+    function weather(lat, long){
         var URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
 
-        $.getJSON(URL, function(data) {
+        $.getJSON(URL, function(data){
+            console.log(data);
             updateDOM(data);
         });
     }
 
-    // Update Dom
-    function updateDOM(data) {
+    function updateDOM(data){
         var city = data.name;
-        var temp = Math.round(data.main.temp_max);
+        var temp = Math.round(data.main.temp);
+        var feels = Math.round(data.main.feels_like)
         var desc = data.weather[0].description;
         var icon = data.weather[0].icon;
 
         $('#city').html(city);
         $('#temp').html(temp);
+        $('#feels').append(feels);
         $('#desc').html(desc);
-        $('#icon').attr('src', icon);
+        $('#icon').attr('src',icon);
     }
+
 });
